3c3fe5bffc1e84ba17b8887258010f6f3c118a99
adnanahid <sikuahamed@gmail.com>
2025-02-22 12:27:28 +0600

    deploy done

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
+//middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://taskly-bbccc.web.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kgmqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -29,7 +36,7 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
-    app.post("/userdetails", async (req, res) => {
+    app.post("/userDetails", async (req, res) => {
       const details = req.body;
       const isExist = await usersCollection.findOne({ email: details.email });
       if (!isExist) {
@@ -72,6 +79,10 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port http://localhost:${PORT}`);
