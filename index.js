fabaa3e4c00fc716317bb578426e0c65b00f34e9
adnanahid <sikuahamed@gmail.com>
2025-02-27 20:35:46 +0600

    fixed some error and final push

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const cors = require("cors");
-const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-//middleware
+// Create HTTP server
+const server = http.createServer(app);
+
+// WebSocket server
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173", "https://taskly-bbccc.web.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
+
+// Middleware
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://taskly-bbccc.web.app", "http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://taskly-bbccc.web.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
+// MongoDB connection
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kgmqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -27,63 +41,60 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-  try {
-    const TaskLy_DB = client.db("TaskLy_DB");
-    const usersCollection = TaskLy_DB.collection("usersCollections");
-    const taskCollection = TaskLy_DB.collection("taskCollection");
+  await client.connect();
+  console.log("Connected to MongoDB!");
 
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+  const TaskLy_DB = client.db("TaskLy_DB");
+  const usersCollection = TaskLy_DB.collection("usersCollections");
+  const taskCollection = TaskLy_DB.collection("taskCollection");
 
-    app.post("/userDetails", async (req, res) => {
-      const details = req.body;
-      const isExist = await usersCollection.findOne({ email: details.email });
-      if (!isExist) {
-        const result = await usersCollection.insertOne(details);
-        return res.send(result);
-      } else {
-        return res.send("already exist");
-      }
-    });
+  // ** User Registration **
+  app.post("/userDetails", async (req, res) => {
+    const details = req.body;
+    const isExist = await usersCollection.findOne({ email: details.email });
 
-    app.post("/tasks", async (req, res) => {
-      const task = req.body;
-      const result = await taskCollection.insertOne(task);
+    if (!isExist) {
+      const result = await usersCollection.insertOne(details);
       return res.send(result);
-    });
+    }
+    return res.send("already exist");
+  });
 
-    app.get("/tasks/:email", async (req, res) => {
-      const query = { email: req.params.email };
-      const result = await taskCollection.find(query).toArray();
-      res.send(result);
-    });
+  // ** Add New Task **
+  app.post("/tasks", async (req, res) => {
+    const task = req.body;
+    const result = await taskCollection.insertOne(task);
+    res.send(result);
+  });
 
-    app.delete("/tasks/:_id", async (req, res) => {
-      const result = await taskCollection.deleteOne({
-        _id: new ObjectId(req.params._id),
-      });
-      res.send("Task deleted successfully");
-    });
+  // ** Get Tasks by Email **
+  app.get("/tasks/:email", async (req, res) => {
+    const query = { email: req.params.email };
+    const result = await taskCollection.find(query).toArray();
+    res.send(result);
+  });
 
-    app.put("/tasks/:id", async (req, res) => {
-      const result = await taskCollection.updateOne(
-        { _id: new ObjectId(req.params.id) },
-        { $set: req.body }
-      );
-      res.send(result);
+  // ** Delete Task **
+  app.delete("/tasks/:_id", async (req, res) => {
+    const result = await taskCollection.deleteOne({
+      _id: new ObjectId(req.params._id),
     });
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    res.send("Task deleted successfully");
+  });
+
+  // ** Update Task **
+  app.put("/tasks/:id", async (req, res) => {
+    const result = await taskCollection.updateOne(
+      { _id: new ObjectId(req.params.id) },
+      { $set: req.body }
+    );
+    res.send(result);
+  });
 }
+
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
+// Start Server
 app.listen(PORT, () => {
-  console.log(`Example app listening on port http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
